bceaabd5f8aa596768b6359137773d841e7653af
Rhea <66195574+rjoh10@users.noreply.github.com>
2024-01-26 19:07:14 -0500

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,15 @@
+function goToMusicPage() {
+    // Replace 'music.html' with the actual filename of your music page
+    window.location.href = 'musicPage.html';
+}
+
+// Function to navigate back to the main page
+function goToNextPage() {
+    // Replace 'index.html' with the actual filename of your main page
+    window.location.href = 'nextPage.html';
+}
+
+function goToCafePage() {
+    // Replace 'index.html' with the actual filename of your main page
+    window.location.href = 'cafePage.html';
+} 
